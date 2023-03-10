24a16bc94c1817c678a4cc8ba1c14c9156052d23
tom-shimoda <shimoda0204@gmail.com>
2023-03-10 14:11:47 +0900

    first commit.

---
+++ Main.cs
@@ -0,0 +1,69 @@
+﻿class Negaimashiteha
+{
+    static readonly int numCount = 5;
+    static readonly int numRangeMax = 1000;
+    static readonly int numRangeMin = 0;
+
+    static void Main()
+    {
+        Random rnd = new Random();
+        ConsoleKeyInfo input;
+        int questionIndex = 1;
+
+        do
+        {
+            List<int> nums = new List<int>();
+            int answer = 0;
+
+            // 問題生成
+            for (int i = 0; i < numCount; i++)
+            {
+                var num = rnd.Next(numRangeMin, numRangeMax);
+                nums.Add(num);
+                answer += num;
+            }
+
+            // 問題表示
+            Console.WriteLine($"Q{questionIndex}:");
+            foreach (var v in nums)
+            {
+                Console.WriteLine($"{v}");
+            }
+            Console.WriteLine("?");
+
+            // 回答処理
+            string guess = "";
+            do
+            {
+                input = Console.ReadKey(true);
+                if (input.Key != ConsoleKey.Enter && input.KeyChar >= '0' && input.KeyChar <= '9')
+                {
+                    guess += input.KeyChar;
+                    Console.Write(input.KeyChar);
+                }
+            } while (input.Key != ConsoleKey.Enter && input.Key != ConsoleKey.Escape);
+
+            Console.WriteLine();
+
+            if (input.Key == ConsoleKey.Enter)
+            {
+                if (int.Parse(guess) == answer)
+                {
+                    Console.WriteLine("正解です！");
+                }
+                else
+                {
+                    Console.WriteLine("不正解です。正解は " + answer + " です。");
+                }
+                Console.WriteLine("--------------------------");
+            }
+            else // ESCキーが押された場合
+            {
+                Console.WriteLine("問題を終了します。");
+                break;
+            }
+
+            questionIndex++;
+        } while (true);
+    }
+}
